e72929152b6a9dc4705f75562860d529153228da
BoldWarriorHS <51426882+BoldWarriorHS@users.noreply.github.com>
2019-07-26 10:48:37 -0700

    Update Satisfactory Backerupper.bat

--- Satisfactory Backerupper.bat
+++ Satisfactory Backerupper.bat
@@ -1,5 +1,4 @@
 SET currentsave="%localappdata%\FactoryGame\Saved\SaveGames"
 SET backuplocation="%homedrive%%homepath%\OneDrive\Satisfactory Saves"
 IF NOT EXIST %backuplocation% mkdir %backuplocation%
-cd %backuplocation%
 COPY /y %currentsave%\* %backuplocation%
