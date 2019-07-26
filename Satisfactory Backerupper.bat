10213044023acd2d6ccc896f6ddde09e47c1e70f
BoldWarriorHS <51426882+BoldWarriorHS@users.noreply.github.com>
2019-07-26 10:47:31 -0700

    Update main code
    enhancements to make the code work for more people out-of-box. Thanks u/kaceymonk

--- Satisfactory Backerupper.bat
+++ Satisfactory Backerupper.bat
@@ -1,6 +1,5 @@
-SET currentsave="C:\Users\%username%\AppData\Local\FactoryGame\Saved\SaveGames"
-SET backuplocation="C:\Users\%username%\OneDrive\Satisfactory Saves"
+SET currentsave="%localappdata%\FactoryGame\Saved\SaveGames"
+SET backuplocation="%homedrive%%homepath%\OneDrive\Satisfactory Saves"
 IF NOT EXIST %backuplocation% mkdir %backuplocation%
 cd %backuplocation%
-SET COPYCMD=/Y
-COPY %currentsave%\*
+COPY /y %currentsave%\* %backuplocation%
