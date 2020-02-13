57b3eb78dd6c551f6636e80923afa2405358d5b9
BoldWarriorHS <51426882+BoldWarriorHS@users.noreply.github.com>
2020-02-13 08:47:38 -0800

    Remove outdated version of the script

--- Satisfactory Backerupper.bat
+++
@@ -1,4 +0,0 @@
-SET currentsave="%localappdata%\FactoryGame\Saved\SaveGames"
-SET backuplocation="%homedrive%%homepath%\OneDrive\Satisfactory Saves"
-IF NOT EXIST %backuplocation% mkdir %backuplocation%
-COPY /y %currentsave%\* %backuplocation%
